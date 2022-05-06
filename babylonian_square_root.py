a27a1542fb2ac7d3324ee907dac99f59793f4222
eshapatel13 <eshapatel@my.unt.edu>
2022-05-06 17:10:43 -0500

    Add files via upload

---
+++ babylonian_square_root.py
@@ -0,0 +1,28 @@
+# find the square root of a number using the babylonian method
+# x is the number we're trying to find the square root of
+# e is the estimate
+# epsilon is 0.00001
+# |x/e - e| < epsilon means that we have a good enough estimate
+# (x/e + e)/2 is the new estimate
+# use a while loop to repeat the process till we can find the square root
+
+# set value of epsilon
+epsilon = 0.0001
+# get input from user
+x = int(input("Enter a positive integer value: "))
+# set e equal to x
+e = float(x)
+# while loop in order to perform the calculations and repeat them
+while abs((x/e) - e) > epsilon:
+    if x <= 0:
+        print("Your number must be a positive integer. Please try again.")
+        x = int(input("Enter a positive integer value: "))
+        e = float(x)
+    else:
+        e = ((x / e) + e) / 2
+
+# round e to 3 decimal places
+e = format(e,'.3f')
+# print the values
+print("Value Square Root")
+print(x,e,sep='     ')
